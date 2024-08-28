b723a30f0cf9a497dc6537294234496a8bd3e223
c-punturieri <claire.punturieri@gmail.com>
2024-08-28 12:01:08 -0500

    Update mak_features to lab standards, minor edits
    - Changed files to be representative of study_gps
    - Added in more context_values for "type" feature

--- notebooks/mak_features_chtc.R
+++ notebooks/mak_features_chtc.R
@@ -1,17 +1,25 @@
 # Script to engineer features on CHTC
 
 # Constants
-dist_max <- 50   # only use context if places are within 50 meters
+# CP: do we need either of these?
+dist_max <- 0.31   # only use context if places are within 50 meters (0.31 miles)
 window <- "1day"  #window for calculating labels
+
+options(conflicts.policy = "depends.ok")
+devtools::source_url("https://github.com/jjcurtin/lab_support/blob/main/format_path.R?raw=true", 
+                     sha1 = "a58e57da996d1b70bb9a5b58241325d6fd78890f")
+
+path_gps <- format_path("studydata/risk/data_processed/gps")
 
 suppressPackageStartupMessages({
   library(dplyr)
   library(lubridate)
-  library(vroom)
+  library(readr)
   library(foreach)
-  source("fun_features.R")
+  source("../analysis_risk/shared/fun_features.R")
 })
 
+# CP: do we need this?
 # get chtc process num
 args <- commandArgs(trailingOnly = TRUE) 
 #job_start <- 2001
@@ -20,21 +28,35 @@ job_start <- as.numeric(args[1]) # CHTC arg starts at 1 because using passed in 
 job_stop <- as.numeric(args[2])
 
 # read in data
-data <- vroom("data.csv.xz", show_col_types = FALSE)%>% 
+# CP: instead of reaading in this file, I will change this so that the file is created here
+# just leaving at as this for now while I am testing out code
+data <- read_csv(here::here(path_gps, "gps_enriched.csv.xz"), show_col_types = FALSE) |>
+  # variable conversions
   mutate(time = with_tz(time, tz = "America/Chicago"),
-         duration = duration / 60) %>% #convert minutes to hours because period duration units are hours
-  rename(dttm_obs = time) %>% 
-  filter(dist_context <= dist_max) # currently only care about observations with context
+         dist = dist / 1609.344,
+         duration = duration / 60,
+         speed = dist / duration,
+         dist_context = dist_context / 1609.344) |>
+  rename(dttm_obs = time) |> 
+  # filtering, see: eda_places
+  mutate(duration = if_else(dist > 0.01 & duration == 0, NA_real_, duration),
+         duration = if_else(speed > 100, NA_real_, duration),
+         duration = if_else(duration > 2 & dist > 0.31, NA_real_, duration),
+         duration = if_else(duration > 24, 24, duration),
+         known_loc = if_else(dist_context <= 0.031 & speed <= 4, TRUE, FALSE),
+         known_loc = if_else(is.na(known_loc), FALSE, known_loc)) |> 
+  filter(known_loc == TRUE) # currently only care about observations with context
                                    # this may change when need to figure out total duration
                                    # of observations in window in future
 
-labels <- vroom("labels.csv", show_col_types = FALSE) %>% 
-  mutate(dttm_label = with_tz(dttm_label, tz = "America/Chicago")) %>% 
-  slice(job_start:job_stop) %>% 
-  mutate(label_num = seq(job_start, job_stop, by = 1))
+labels <- read_csv(here::here(path_gps, "lapses.csv"), show_col_types = FALSE) |>  
+  mutate(dttm_label = with_tz(lapse_onset, tz = "America/Chicago")) #|>  
+  # CP: commented out, not sure if we need this if we are not using chtc  
+  #slice(job_start:job_stop) |> 
+  #mutate(label_num = seq(job_start, job_stop, by = 1))
 
-dates <- vroom("study_dates.csv", show_col_types = FALSE) %>% 
-  select(subid, data_start = study_start) %>% 
+dates <- read_csv(here::here(path_gps, "study_dates.csv"), show_col_types = FALSE)  |>  
+  select(subid, data_start = study_start) |>  
   mutate(data_start = with_tz(data_start, tz = "America/Chicago"))
 
 
@@ -43,14 +65,28 @@ period_durations <- c(6, 12, 24, 48, 72, 168)
 lead <-  0 
 
 # make features ------------------
-# i_label <- 1   # for testing
+# CP: iterates through each subject by day. each feature_row represents
+# all calculations for every period duration for that place feature and
+# each possible response type for that feature.
+# row: day of observation
+# column: every possible rate calculated for each response type and duration combination
+# three types of rates are returned from score_ratesum:
+# a raw rate and two relative rates (these are our change rate features):
+# dratecount: difference between rate in period and rate across all data
+# pratecount: percent change between rate in period and rate across all data
+# Q: what is the benefit of having a difference rate and a percent change rate?
+# it doesn't actually look like pratecount is getting calculated right now
+
+# CP: uncommented line 81 because it said for testing
+i_label <- 1   # for testing
 features <- foreach (i_label = 1:nrow(labels), .combine = "rbind") %do% {
   
   # for (the_label_num in job_start:job_stop) {
-  label <- labels %>% slice(i_label)
+  label <- labels |> slice(i_label)
   subid <- label$subid 
   dttm_label <-  label$dttm_label
-  the_label_num <- label$label_num
+  #the_label_num <- label$label_num # commented out by CP, wasn't sure what this does
+  the_label_num <- 1 # added by CP to make things work temporarily
   
   # type
   feature_row <- score_ratesum(subid, 
@@ -61,13 +97,16 @@ features <- foreach (i_label = 1:nrow(labels), .combine = "rbind") %do% {
                                data_start = dates, 
                                col_name = "duration", 
                                context_col_name = "type",
-                               context_values = c("aa", "bar", "cafe", "church", "family",
-                                                  "fitness", "healthcare", "home",
-                                                  "liquorstore", "park", "restaurant",
-                                                  "school", "volunteer", "work"))
+                               # CP: added errands, friend, and other
+                               context_values = c("aa", "bar", "cafe", "church",
+                                                  "errands", "family", "fitness",
+                                                  "friend", "healthcare", "home",
+                                                  "liquorstore", "other", "park",
+                                                  "restaurant", "school",
+                                                  "volunteer", "work"))
   
   # drank
-  feature_row <- feature_row %>% 
+  feature_row <- feature_row |>  
     full_join(score_ratesum(subid, 
                             dttm_label,
                             x_all  = data,
@@ -80,7 +119,7 @@ features <- foreach (i_label = 1:nrow(labels), .combine = "rbind") %do% {
               by = c("subid", "dttm_label"))
   
   # alcohol
-  feature_row <- feature_row %>% 
+  feature_row <- feature_row |>  
     full_join(score_ratesum(subid, 
                             dttm_label,
                             x_all  = data,
@@ -93,7 +132,7 @@ features <- foreach (i_label = 1:nrow(labels), .combine = "rbind") %do% {
               by = c("subid", "dttm_label"))
   
   # emotion
-  feature_row <- feature_row %>% 
+  feature_row <- feature_row |>  
     full_join(score_ratesum(subid, 
                             dttm_label,
                             x_all  = data,
@@ -106,7 +145,7 @@ features <- foreach (i_label = 1:nrow(labels), .combine = "rbind") %do% {
               by = c("subid", "dttm_label"))
   
   # risk
-  feature_row <- feature_row %>% 
+  feature_row <- feature_row |>  
     full_join(score_ratesum(subid, 
                             dttm_label,
                             x_all  = data,
@@ -119,7 +158,7 @@ features <- foreach (i_label = 1:nrow(labels), .combine = "rbind") %do% {
               by = c("subid", "dttm_label"))
   
   # avoid
-  feature_row <- feature_row %>% 
+  feature_row <- feature_row |> 
     full_join(score_ratesum(subid, 
                             dttm_label,
                             x_all  = data,
@@ -131,14 +170,15 @@ features <- foreach (i_label = 1:nrow(labels), .combine = "rbind") %do% {
                             context_values = c("yes", "no")), 
               by = c("subid", "dttm_label"))
   
-  feature_row <- feature_row %>% 
+  feature_row <- feature_row |> 
     mutate(label_num = the_label_num)
   
   feature_row
 }
 
 # Add outcome label and other info to features ------------------
-features %>%
-  mutate(lapse = labels$lapse) %>% 
-  relocate(label_num, subid, dttm_label, lapse) %>% 
-  vroom_write(str_c("features_", window, "_", job_start, "_", job_stop, ".csv"), delim = ",")
+features |> 
+  mutate(lapse = labels$label) |>  
+  relocate(label_num, subid, dttm_label, lapse) |>
+  write_csv(here::here(path_gps, "features.csv"))
+  #vroom_write(str_c("features_", window, "_", job_start, "_", job_stop, ".csv"), delim = ",")
