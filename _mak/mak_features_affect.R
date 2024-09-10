c0d7430b63f6305dc1fc3af3e3001b24fb6ede49
c-punturieri <claire.punturieri@gmail.com>
2024-09-10 10:02:19 -0500

    Create script for testing affective features

---
+++ _mak/mak_features_affect.R
@@ -0,0 +1,249 @@
+# Script to engineer features on CHTC
+
+# Constants
+dist_max <- 0.031
+window <- "1day"  #window for calculating labels
+
+devtools::source_url("https://github.com/jjcurtin/lab_support/blob/main/format_path.R?raw=true", 
+                     sha1 = "a58e57da996d1b70bb9a5b58241325d6fd78890f")
+
+path_gps <- format_path("studydata/risk/data_processed/gps")
+
+suppressPackageStartupMessages({
+  library(dplyr)
+  library(lubridate)
+  library(readr)
+  library(foreach)
+  source("../analysis_risk/shared/fun_features.R")
+})
+
+# get chtc process num
+#args <- commandArgs(trailingOnly = TRUE) 
+#job_start <- 2001
+#job_stop <- 2002
+#job_start <- as.numeric(args[1]) # CHTC arg starts at 1 because using passed in row numbers
+#job_stop <- as.numeric(args[2])
+
+# read in data
+# CP: instead of reaading in this file, I will change this so that the file is created here
+# just leaving at as this for now while I am testing out code
+data <- read_csv(here::here(path_gps, "gps_enriched.csv.xz"), show_col_types = FALSE) |>
+  # variable conversions
+  mutate(time = with_tz(time, tz = "America/Chicago"),
+         dist = dist / 1609.344,
+         duration = duration / 60,
+         speed = dist / duration,
+         dist_context = dist_context / 1609.344) |>
+  rename(dttm_obs = time) |> 
+  # filtering, see: eda_places
+  mutate(duration = if_else(dist > 0.01 & duration == 0, NA_real_, duration),
+         duration = if_else(speed > 100, NA_real_, duration),
+         duration = if_else(duration > 2 & dist > 0.31, NA_real_, duration),
+         duration = if_else(duration > 24, 24, duration),
+         known_loc = if_else(dist_context <= dist_max & speed <= 4, TRUE, FALSE),
+         known_loc = if_else(is.na(known_loc), FALSE, known_loc)) # don't filter based on known loc
+
+labels <- read_csv(here::here(path_gps, "labels.csv"), show_col_types = FALSE) |>
+  # dttm_label represents label ONSET, required for score_ratesum function
+  mutate(dttm_label = with_tz(day_start, tz = "America/Chicago"),
+         day_end = with_tz(day_end, tz = "America/Chicago")) #|>  
+#slice(job_start:job_stop) |> 
+#mutate(label_num = seq(job_start, job_stop, by = 1))
+
+dates <- read_csv(here::here(path_gps, "study_dates.csv"), show_col_types = FALSE)  |>  
+  select(subid, data_start = study_start) |>  
+  mutate(data_start = with_tz(data_start, tz = "America/Chicago"))
+
+
+# initialize period durations and lead hours ------------------
+period_durations <- c(6, 12, 24, 48, 72, 168) 
+lead <-  0 
+
+# make features ------------------
+
+
+# feature function test
+score_variance <- function(the_subid, the_dttm_label, x_all, 
+                         period_durations, lead, data_start, 
+                         col_name, data_type_col_name = NA, data_type_values = NA, 
+                         context_col_name = NA, context_values = NA, passive = FALSE) {
+  
+  # Gets value for col_name and returns a raw variance, a variance change, and a proportion change in variance from baseline
+  # i.e., (raw variance - baseline variance) / baseline variance
+  # proportion score set to NA if baseline == 0
+  
+  # the_subid: single integer subid
+  # the_dttm_label: single dttm for label onset
+  # x_all:  raw data for all subids and communications
+  # period_durations: vector of 1+ integer period_durations in hours
+  # lead: the lead time for prediction in hours (a single integer)
+  # data_start: a df with data_start = min(study_start, comm_start) for all subids
+  # col_name: column name for raw data for feature as string - should be continuous var
+  # data_type_col_name: name of column name to filter on for data log type values
+  # data_type_values: a vector of 1+ meta data log types to filter on (sms, or voice, if empty uses all logs)
+  # context_col_name: col_name of context feature. Set to NA if no context filter
+  # context_values: a vector of 1+ context values to filter on.  Set to NA if no context filter
+  # passive: is a variable to distinguish variables that use no context and to append passive
+  # onto those variable names for filtering down feature sets in recipes (set to TRUE if passive)
+  
+  # define period_var function
+  period_var <- function (.x) {
+    if (length(.x) > 0) { 
+      if (!all(is.na(.x))) {
+        the_var <- var(.x, na.rm = TRUE)
+      } else the_var <- NA
+    } else the_var <- NA
+    
+    return(the_var)
+  }
+  
+  # nested foreach - period_duration within data_type_value within context_value
+  
+  features <- foreach (context_value = context_values, .combine = "cbind") %do% {
+    
+    # Filter data if context_value provided
+    if (!is.na(context_value)) {
+      x_c <- x_all %>%
+        filter(.data[[context_col_name]] == context_value) 
+    } else x_c <- x_all
+    
+    
+    foreach (data_type_value = data_type_values, .combine = "cbind") %do% {
+      
+      if (!is.na(data_type_value)) {
+        x <- x_c %>% filter(.data[[data_type_col_name]] == data_type_value) 
+      } else x <- x_c
+      
+      # get baseline variance using all data before label dttm
+      baseline <- x %>% 
+        get_x_period(the_subid, the_dttm_label, x_all = ., lead, period_duration = Inf) %>% # Inf gives all data back to first obs
+        summarise("base" := period_var(.data[[col_name]])) %>% 
+        pull(base)
+      
+      foreach (period_duration = period_durations, .combine = "cbind") %do% {
+        
+        raw <- x %>%
+          get_x_period(the_subid, the_dttm_label, ., lead, period_duration) %>% 
+          summarise("raw" := period_var(.data[[col_name]])) %>%
+          pull(raw)
+        
+        passive_label <- if_else(passive, "passive", "NA")
+        
+        tibble(
+          "{data_type_value}.p{period_duration}.l{lead}.rvar_{col_name}.{context_col_name}.{context_value}.{passive_label}" := raw,
+          "{data_type_value}.p{period_duration}.l{lead}.dvar_{col_name}.{context_col_name}.{context_value}.{passive_label}" := raw - baseline,
+          "{data_type_value}.p{period_duration}.l{lead}.pvar_{col_name}.{context_col_name}.{context_value}.{passive_label}" := (raw - baseline) / baseline) %>% 
+          rename_with(~str_remove_all(.x, ".NA")) %>% 
+          rename_with(~str_remove(.x, "^NA."))
+      }
+    }
+  }
+  
+  features <- features %>%
+    mutate(subid = the_subid,
+           dttm_label = the_dttm_label) %>%
+    relocate(subid, dttm_label)
+  
+  return(features)
+}
+
+
+
+
+labels <- labels |> 
+  slice_head(n = 100)
+
+#i_label <- 1   # for testing
+
+cl <- parallel::makePSOCKcluster(parallel::detectCores(logical = FALSE))
+doParallel::registerDoParallel(cl)
+
+features <- foreach (i_label = 1:nrow(labels),
+                     .combine = "rbind",
+                     .packages = c("dplyr", "foreach", "lubridate", "stringr")) %dopar% {
+                       
+                       # for (the_label_num in job_start:job_stop) {
+                       label <- labels |> slice(i_label)
+                       subid <- label$subid 
+                       dttm_label <-  label$dttm_label
+                       #the_label_num <- label$label_num
+                       
+                       # home stay built into context features
+                       
+                       # transition time
+                       # amount of time in non-stationary state
+                       # this would be when known_loc is false?
+                       # maybe more appropriate to just use speed as a filter?
+                       feature_row <- score_ratesum(subid, 
+                                                    dttm_label,
+                                                    x_all  = data,
+                                                    period_durations = period_durations,
+                                                    lead = lead, 
+                                                    data_start = dates, 
+                                                    col_name = "duration", 
+                                                    context_col_name = "known_loc",
+                                                    context_values = c(TRUE, FALSE))
+                       
+                       # total distance
+                       # trying to repurpose existing functions, would there be any benefit to just summing distance?
+                       feature_row <- feature_row |>  
+                         full_join(score_mean(subid, 
+                                                 dttm_label,
+                                                 x_all  = data,
+                                                 period_durations = period_durations,
+                                                 lead = lead, 
+                                                 data_start = dates, 
+                                                 col_name = "dist"), 
+                                   by = c("subid", "dttm_label"))
+                       
+                       feature_row <- feature_row |>  
+                         full_join(score_median(subid, 
+                                              dttm_label,
+                                              x_all  = data,
+                                              period_durations = period_durations,
+                                              lead = lead, 
+                                              data_start = dates, 
+                                              col_name = "dist"), 
+                                   by = c("subid", "dttm_label"))
+                       
+                       # location variance
+                       feature_row <- feature_row |>  
+                         full_join(
+                           # definitely can't do this
+                           # need to add variances together before calculating rates
+                          log(score_variance(subid, 
+                                                dttm_label,
+                                                x_all  = data,
+                                                period_durations = period_durations,
+                                                lead = lead, 
+                                                data_start = dates, 
+                                                col_name = "lat") +
+                            score_variance(subid, 
+                                             dttm_label,
+                                             x_all  = data,
+                                             period_durations = period_durations,
+                                             lead = lead, 
+                                             data_start = dates, 
+                                             col_name = "lon") 
+                                          ), 
+                                   by = c("subid", "dttm_label"))
+                       
+                       # n clusters -- could use number of contexts collected
+                       
+                       # normalized entropy
+                       
+                       # circadian rhythm
+                       
+                       feature_row
+                     }
+
+# Quick EDA -----------
+
+## note to self to add in some EDA
+
+# Add outcome label and other info to features ------------------
+features |> 
+  mutate(lapse = labels$lapse,
+         label_num = 1:nrow()) |>  
+  relocate(label_num, subid, dttm_label, lapse) |>
+  write_csv(here::here(path_gps, "features.csv"))
